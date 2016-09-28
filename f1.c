b78276dc2ef821872159ef717a905f28124e26e3
shannabish <bishopsm@dukes.jmu.edu>
2016-09-28 18:09:58 -0400

    update in branch

--- f1.c
+++ f1.c
@@ -4,3 +4,9 @@ main()
 	printf("and the presence of absence\n);
 	printf("website not found!\n");
 }
+feature1()
+{
+	printf("no available space\n");
+	printf("Look to the cloud");
+	printf("your drive is full:\n");
+}
